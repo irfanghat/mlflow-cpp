bc0b308c902f3c3a9701b010a613f6435eb56167
irfanghat <irfanghat@gmail.com>
2026-06-27 15:35:59 +0000

    Add clang-format

--- tests/test_experiments.cpp
+++ tests/test_experiments.cpp
@@ -1,111 +1,119 @@
 #include "client_fixture.hpp"
 #include "mlflow/models/experiment.hpp"
 
-TEST_F(MlflowCppClientFixture, CreateExperiment) {
-  std::string name = unique_name("create_experiment");
-
-  auto res = client.create_experiment(name);
-
-  ASSERT_TRUE(res.success);
-  EXPECT_FALSE(res.data.empty());
-}
-
-TEST_F(MlflowCppClientFixture, CreateExperimentDuplicateShouldFail) {
-  std::string name = unique_name("experiment_duplicate");
-
-  auto first = client.create_experiment(name);
-  ASSERT_TRUE(first.success);
-
-  auto second = client.create_experiment(name);
-
-  EXPECT_FALSE(second.success);
-  EXPECT_TRUE(second.data.empty() || second.data == "");
-  EXPECT_NE(second.error_message.find("HTTP"), std::string::npos);
-}
-
-TEST_F(MlflowCppClientFixture, CreateMultipleExperiments) {
-  for (int i = 0; i < 5; i++) {
-    auto name = unique_name("bulk_exp_" + std::to_string(i));
+TEST_F(MlflowCppClientFixture, CreateExperiment)
+{
+    std::string name = unique_name("create_experiment");
 
     auto res = client.create_experiment(name);
 
     ASSERT_TRUE(res.success);
     EXPECT_FALSE(res.data.empty());
-  }
 }
 
-TEST_F(MlflowCppClientFixture, CreateExperimentWithArtifactLocation) {
-  std::string name = unique_name("experiment_artifact_location");
-  std::string artifact_location = "./custom_artifact_location";
-  auto res = client.create_experiment(name, artifact_location);
+TEST_F(MlflowCppClientFixture, CreateExperimentDuplicateShouldFail)
+{
+    std::string name = unique_name("experiment_duplicate");
 
-  ASSERT_TRUE(res.success) << "Failed to create experiment: "
-                           << res.error_message;
-  EXPECT_FALSE(res.data.empty());
+    auto first = client.create_experiment(name);
+    ASSERT_TRUE(first.success);
+
+    auto second = client.create_experiment(name);
+
+    EXPECT_FALSE(second.success);
+    EXPECT_TRUE(second.data.empty() || second.data == "");
+    EXPECT_NE(second.error_message.find("HTTP"), std::string::npos);
 }
 
-TEST_F(MlflowCppClientFixture, CreateExperimentWithTags) {
-  std::string artifact_location = "./custom_artifact_location";
-  std::string name = unique_name("experiment_with_tag");
-  std::vector<mlflow::ExperimentTag> tags = {
-      mlflow::ExperimentTag{"Test Tag Key", "Test Tag Value"}};
-  auto res = client.create_experiment(name, artifact_location, tags);
+TEST_F(MlflowCppClientFixture, CreateMultipleExperiments)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        auto name = unique_name("bulk_exp_" + std::to_string(i));
 
-  ASSERT_TRUE(res.success) << "Failed to create experiment: "
-                           << res.error_message;
-  EXPECT_FALSE(res.data.empty());
+        auto res = client.create_experiment(name);
+
+        ASSERT_TRUE(res.success);
+        EXPECT_FALSE(res.data.empty());
+    }
 }
 
-TEST_F(MlflowCppClientFixture, GetExperimentByID) {
-  std::string name = unique_name("get_experiment");
+TEST_F(MlflowCppClientFixture, CreateExperimentWithArtifactLocation)
+{
+    std::string name = unique_name("experiment_artifact_location");
+    std::string artifact_location = "./custom_artifact_location";
+    auto res = client.create_experiment(name, artifact_location);
 
-  auto exp_res = client.create_experiment(name);
-  auto exp_id = exp_res.data;
-
-  ASSERT_TRUE(exp_res.success);
-  EXPECT_FALSE(exp_res.data.empty());
-
-  auto res = client.get_experiment_by_id(exp_id);
-  ASSERT_TRUE(res.success);
-  EXPECT_FALSE(res.data.empty());
+    ASSERT_TRUE(res.success) << "Failed to create experiment: " << res.error_message;
+    EXPECT_FALSE(res.data.empty());
 }
 
-TEST_F(MlflowCppClientFixture, GetExperimentByName) {
-  std::string name = unique_name("get_experiment");
-  auto exp_res = client.create_experiment(name);
+TEST_F(MlflowCppClientFixture, CreateExperimentWithTags)
+{
+    std::string artifact_location = "./custom_artifact_location";
+    std::string name = unique_name("experiment_with_tag");
+    std::vector<mlflow::ExperimentTag> tags = {
+        mlflow::ExperimentTag{"Test Tag Key", "Test Tag Value"}};
+    auto res = client.create_experiment(name, artifact_location, tags);
 
-  ASSERT_TRUE(exp_res.success);
-  EXPECT_FALSE(exp_res.data.empty());
-
-  auto res = client.get_experiment_by_name(name);
-  ASSERT_TRUE(res.success);
-  EXPECT_FALSE(res.data.empty());
+    ASSERT_TRUE(res.success) << "Failed to create experiment: " << res.error_message;
+    EXPECT_FALSE(res.data.empty());
 }
 
-TEST_F(MlflowCppClientFixture, DeleteExperiment) {
-  std::string name = unique_name("delete_experiment");
-  auto exp_res = client.create_experiment(name);
+TEST_F(MlflowCppClientFixture, GetExperimentByID)
+{
+    std::string name = unique_name("get_experiment");
 
-  ASSERT_TRUE(exp_res.success);
-  EXPECT_FALSE(exp_res.data.empty());
+    auto exp_res = client.create_experiment(name);
+    auto exp_id = exp_res.data;
 
-  auto res = client.delete_experiment(exp_res.data);
-  ASSERT_TRUE(res.success);
-  ASSERT_TRUE(res.data.empty());
+    ASSERT_TRUE(exp_res.success);
+    EXPECT_FALSE(exp_res.data.empty());
+
+    auto res = client.get_experiment_by_id(exp_id);
+    ASSERT_TRUE(res.success);
+    EXPECT_FALSE(res.data.empty());
 }
 
-TEST_F(MlflowCppClientFixture, RestoreExperiment) {
-  std::string name = unique_name("restore_experiment");
-  auto exp_res = client.create_experiment(name);
+TEST_F(MlflowCppClientFixture, GetExperimentByName)
+{
+    std::string name = unique_name("get_experiment");
+    auto exp_res = client.create_experiment(name);
 
-  ASSERT_TRUE(exp_res.success);
-  EXPECT_FALSE(exp_res.data.empty());
+    ASSERT_TRUE(exp_res.success);
+    EXPECT_FALSE(exp_res.data.empty());
 
-  auto del_exp_res = client.delete_experiment(exp_res.data);
-  ASSERT_TRUE(del_exp_res.success);
-  ASSERT_TRUE(del_exp_res.data.empty());
+    auto res = client.get_experiment_by_name(name);
+    ASSERT_TRUE(res.success);
+    EXPECT_FALSE(res.data.empty());
+}
 
-  auto res = client.restore_experiment(exp_res.data);
-  ASSERT_TRUE(res.success);
-  ASSERT_TRUE(res.data.empty());
+TEST_F(MlflowCppClientFixture, DeleteExperiment)
+{
+    std::string name = unique_name("delete_experiment");
+    auto exp_res = client.create_experiment(name);
+
+    ASSERT_TRUE(exp_res.success);
+    EXPECT_FALSE(exp_res.data.empty());
+
+    auto res = client.delete_experiment(exp_res.data);
+    ASSERT_TRUE(res.success);
+    ASSERT_TRUE(res.data.empty());
+}
+
+TEST_F(MlflowCppClientFixture, RestoreExperiment)
+{
+    std::string name = unique_name("restore_experiment");
+    auto exp_res = client.create_experiment(name);
+
+    ASSERT_TRUE(exp_res.success);
+    EXPECT_FALSE(exp_res.data.empty());
+
+    auto del_exp_res = client.delete_experiment(exp_res.data);
+    ASSERT_TRUE(del_exp_res.success);
+    ASSERT_TRUE(del_exp_res.data.empty());
+
+    auto res = client.restore_experiment(exp_res.data);
+    ASSERT_TRUE(res.success);
+    ASSERT_TRUE(res.data.empty());
 }
